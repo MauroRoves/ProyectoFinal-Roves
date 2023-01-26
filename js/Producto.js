69d7dad9648da3ed33e9664bcbd23ec83db9e4cb
Mauro Roves <mauro.roves@gmail.com>
2023-01-26 18:56:36 -0300

    se mejoro el modal, se agrego JSON

--- js/Producto.js
+++ js/Producto.js
@@ -1,13 +1,15 @@
 
-const productos = [
-    {id: 1, nombre: "Torre de Pisa", tamaño: "a4", precio: 10, tecnica: "fibra", img: '../imagenesCarrito/torrePisa.jpg'},
-    {id: 2, nombre: "Viaje de Chihiro", tamaño: "a4", precio: 30, tecnica: "fibra", img: '../imagenesCarrito/chihiro.jpg'},
-    {id: 3, nombre: "Torre Eiffel", tamaño: "a4", precio: 10, tecnica: "fibra", img: '../imagenesCarrito/torreEiffel.jpg'},
-    {id: 4, nombre: "Casa en Punta Negra", tamaño: "a4", precio: 15, tecnica: "fibra y marcador", img: '../imagenesCarrito/puntaNegra.jpg'},
-    {id: 5, nombre: "Living con estufa a leña", tamaño: "a4", precio: 10, tecnica: "fibra y marcador", img: '../imagenesCarrito/FondoCasa.jpg'},
-    {id: 6, nombre: "Cuatro de Espadas", tamaño: "a3", precio: 10, tecnica: "Acrilico", img: '../imagenesCarrito/carta.jpg'},
-    {id: 7, nombre: "Patron sobre lienzo", tamaño: "a3", precio: 30, tecnica: "Sellos y Pintura de Tela", img: '../imagenesCarrito/lienzo1.jpg'},
-    {id: 8, nombre: "Patron con anomalia", tamaño: "a3", precio: 10, tecnica: "Acrilico", img: '../imagenesCarrito/patron.jpg'},
+const stockProductos = [
+    {id: 1, nombre: "Torre de Pisa", tamanio: "a4", precio: 10, tecnica: "fibra", img: '../imagenesCarrito/torrePisa.jpg'},
+    {id: 2, nombre: "Viaje de Chihiro", tamanio: "a4", precio: 30, tecnica: "fibra", img: '../imagenesCarrito/chihiro.jpg'},
+    {id: 3, nombre: "Torre Eiffel", tamanio: "a4", precio: 10, tecnica: "fibra", img: '../imagenesCarrito/torreEiffel.jpg'},
+    {id: 4, nombre: "Casa en Punta Negra", tamanio: "a4", precio: 15, tecnica: "fibra y marcador", img: '../imagenesCarrito/puntaNegra.jpg'},
+    {id: 5, nombre: "Living con estufa a leña", tamanio: "a4", precio: 10, tecnica: "fibra y marcador", img: '../imagenesCarrito/FondoCasa.jpg'},
+    {id: 6, nombre: "Cuatro de Espadas", tamanio: "a3", precio: 10, tecnica: "Acrilico", img: '../imagenesCarrito/carta.jpg'},
+    {id: 7, nombre: "Patron sobre lienzo", tamanio: "a3", precio: 30, tecnica: "Sellos y Pintura de Tela", img: '../imagenesCarrito/lienzo1.jpg'},
+    {id: 8, nombre: "Patron con anomalia", tamanio: "a3", precio: 10, tecnica: "Acrilico", img: '../imagenesCarrito/patron.jpg'},
 ]
 
 
+
+    
--- js/site.js
+++ js/site.js
@@ -1,9 +1,230 @@
 
 
-const contenedorProductos = document.getElementById("contenedor-productos");
 
+const contenedor = document.querySelector("#contenedor")
+
+let carrito = [];
+
+stockProductos.forEach((prod) => {
+    const {id, nombre, tamanio, precio, tecnica, img} = prod
+    contenedor.innerHTML += `
+                            <div class="card text-bg-dark border-dark mx-5" style="width: 14rem;">
+                                    <img src="${img}" class="card-img-top border-dark rounded img-fluid" alt="torre de pizza">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${nombre}</h5>
+                                        <p class="card-text">Tecnica: ${tecnica}</p>
+                                        <p class="card-text">Tamaño: ${tamanio}</p>
+                                        <p class="card-text">Precio: ${precio}</p>
+                                        <p class="card-text">Numero: ${id}</p>
+                                        <button onclick ="agregarProducto(${id})" class="btn btn-outline-light botones">Comprar</a>
+                                    
+                                    </div>
+                            </div>    
+    `
+})
+
+
+function agregarProducto(id){
+    const item = stockProductos.find((prod) => prod.id === id)
+    carrito.push(item);
+    mostrarCarrito();
+}
+
+
+ const mostrarCarrito = () => {
+    const modalBody = document.querySelector(`.modal .modal-body`);
+    
+    carrito.forEach((prod) => {
+        const {id, nombre, tamanio, precio, tecnica, img} = prod
+        modalBody.innerHTML += `
+                                <div class = "modal-contenedor">
+                                <div>
+                                    <img class="img-fluid" img-carrito" src="${img}"/>
+                                </div>
+
+                                <div>
+                                <p>Producto: ${nombre}</p>
+                                <p>Precio: ${precio}</p>
+                                <p>Numero: ${id}</p>
+
+                                <button class="btn btn-danger">Eliminar Producto</button>
+                                </div>
+
+
+                                
+
+                                
+
+        
+        
+        `
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//PRYECTO ANTERIOR
+
+
+
+/* 
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(response => response.json())
+.then(json => console.log(json))
+
+
+
+
+
+
+let boton = document.querySelector(".btn");
+boton.addEventListener('click', () => {
+   
+    Swal.fire({
+        title: 'Está seguro de eliminar el producto?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, seguro',
+        cancelButtonText: 'No, no quiero'
+    }).then((result) => {
+
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Borrado!',
+                icon: 'success',
+                text: 'El archivo ha sido borrado'
+            })
+        }
+    })
+})
+
+ */
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+/* 
+productos.forEach((producto) => {
+    const compra = document.querySelector(".card");
+    const imgCompra = document.querySelector(".imgItem");
+    imgCompra.classList.add("producto");
+
+    contenedorProductos.appendChild(compra);    
+    
+    const boton = document.getElementById(`agregar${producto.id}`)
+    
+    boton.addEventListener(`click`, () => {
+        agregarAlCarrito(producto.id)
+    })
+                    
+})
+ */
+
+
+
+
+
+/* const contenedorProductos = document.getElementById("contenedor-productos");
 const contenedorCarrito = document.getElementById("contenedor-carrito")
 let carrito = [];
+
 
 
 productos.forEach((producto) => {
@@ -15,6 +236,7 @@ productos.forEach((producto) => {
                         <p> Precio: ${producto.precio} USD </p>
 
                         <button id="agregar${producto.id}">Agregar</button>`
+                        
 
     contenedorProductos.appendChild(compra);    
     
@@ -27,7 +249,9 @@ productos.forEach((producto) => {
 })
  
 
-const agregarAlCarrito = (prodId) => {
+
+
+ const agregarAlCarrito = (prodId) => {
     const item = productos.find((el) => el.id === prodId);
     carrito.push(item);
     actualizarCarrito();
@@ -54,7 +278,52 @@ const agregarAlCarrito = (prodId) => {
         localStorage.setItem("carrito", JSON.stringify(carrito));
     })
 }
- 
+
+
+
+
+boton.addEventListener('click', () => {  
+    Swal.fire({
+        title: 'Está seguro de eliminar el producto?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, seguro',
+        cancelButtonText: 'No, no quiero'
+    }).then((result) => {
+
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Borrado!',
+                icon: 'success',
+                text: 'El archivo ha sido borrado'
+            })
+        }
+    })
+})
+
+
+
+
+productos.forEach((producto) => {
+    const compra = document.createElement("div");
+    compra.classList.add("producto")
+    compra.innerHTML = `<img class="tamanioImgCarrito" src=${producto.img}
+                        <p> Nombre: ${producto.nombre} </p>
+                        <h3> id: ${producto.id} </h3>
+                        <p> Precio: ${producto.precio} USD </p>
+
+                        <button id="agregar${producto.id}">Agregar</button>`
+                        
+
+    contenedorProductos.appendChild(compra);    
+    
+    const boton = document.getElementById(`agregar${producto.id}`)
+    
+    boton.addEventListener(`click`, () => {
+        agregarAlCarrito(producto.id)
+    })
+                    
+})
 
 
 
@@ -66,24 +335,7 @@ const agregarAlCarrito = (prodId) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ */
 
 
 
